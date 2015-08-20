7528e01783bc6552c7274c01bb45b090b7537421
masterUNG <phrombutr@gmail.com>
2015-08-20 10:17:01 +0700

    Start

---
+++ android/src/appEWTC/masterUNG/Thai/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package appEWTC.masterUNG.Thai.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import appEWTC.masterUNG.Thai.MyGdxGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyGdxGame(), config);
+	}
+}
